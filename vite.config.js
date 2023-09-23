cf85303f1c99798db23821065c4ab9b0cc34f0f0
Eric Munyi <irerieric@gmail.com>
2023-09-23 11:34:26 +0300

    vite

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listen on all available network interfaces
+    port: process.env.PORT || 3000, // Use the PORT environment variable or default to 3000
+  },
 })
+
